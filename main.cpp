94dbc47c1499154f45447ebb199ae60e3ef96e04
Agathe Buchert <agathe.buchert@etu.minesparis.psl.eu>
2025-10-30 14:18:50 +0100

    création de main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    while (i < argc)
+    {
+        std::cout << argv[i] << std::endl;
+        i = i + 1;
+    }
+
+    return 0;
+}
