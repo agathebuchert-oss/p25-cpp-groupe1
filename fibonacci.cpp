eaa691a4fb2110d2ca1cbdf51e61f7f1d77d0a10
Agathe Buchert <agathe.buchert@etu.minesparis.psl.eu>
2025-11-04 11:29:33 +0100

    correction de fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,19 +2,42 @@
 
 int str_to_int(char a[])
 {
-    int n = a[0] - '0';
+    int s = 0;
+
+    for (int i = 0; a[i] != '\0'; i++)
+    {
+        if (a[i] < '0' || a[i] > '9')
+        {
+            return -1;
+        }
+    }
+    int n = 0;
+    for (int i = 0; a[i] != '\0'; i++)
+    {
+        n = n * 10 + a[i] - '0';
+    }
     return n;
 }
 
-int main(int agrc, char *argv[])
+int main(int argc, char *argv[])
 {
+    if (argc == 1)
+    {
+        std::cout << "not enough arguments" << std::endl;
+        return 0;
+    }
     int n = str_to_int(argv[1]);
+    if (n == -1)
+    {
+        std::cout << "arguments are not correct" << std::endl;
+        return 0;
+    }
     int f = 0;
     for (int i = 0; i <= n; i++)
     {
         f = f + i;
     }
-    std::cout << "le résulat est " << f << std::endl;
+    std::cout << "le résultat est " << f << std::endl;
 
     return 0;
 }
